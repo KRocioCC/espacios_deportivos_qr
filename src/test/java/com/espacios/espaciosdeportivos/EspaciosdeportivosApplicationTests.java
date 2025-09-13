486b963d5fb194ee331427847575999d0aa960e7
KRocioCC <krociocalderon@gmail.com>
2025-09-13 08:32:39 -0400

    base

---
+++ src/test/java/com/espacios/espaciosdeportivos/EspaciosdeportivosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espacios.espaciosdeportivos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspaciosdeportivosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
